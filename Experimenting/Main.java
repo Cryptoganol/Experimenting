c62e31b08fd57a9119d5114b2d804e3396c2f2b0
Kynan McCabe-Wild <kynanmw@gmail.com>
2022-03-26 06:36:30 -0500

    Created the first program Main

---
+++ Experimenting/Main.java
@@ -0,0 +1,10 @@
+// Kynan McCabe-Wild
+// 3.26.2022
+// Experimenting
+// This is where I can test out things for studying
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
